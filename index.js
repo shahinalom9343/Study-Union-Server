a4c8b2b638ffb831c2d8b57df4705ed1d57297c8
shahinalom9343 <cuet.shahin.eee12@gmail.com>
2024-05-11 20:21:19 +0600

    Deleted function of an assignment done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +32,15 @@ async function run() {
     app.get("/assignments", async(req,res)=>{
       const cursor = studyCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // delete assignment
+    app.delete("/assignments/:id",async(req,res)=>{
+      const id = req.params.id;
+      // console.log(id);
+      const query ={_id: new ObjectId(id)};
+      const result = await studyCollection.deleteOne(query);
       res.send(result);
     })
 
